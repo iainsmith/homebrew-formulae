0dd802574602c5ff013d08a31fb9343635cf3959
Iain Smith <iainsmith@users.noreply.github.com>
2020-04-30 22:17:44 +0100

    Release 0.4.0 of swift-docker

--- Formula/swift-docker.rb
+++ Formula/swift-docker.rb
@@ -1,8 +1,8 @@
 class SwiftDocker < Formula
   desc "Test your swift packages with one command `swift docker test`"
   homepage "https://github.com/iainsmith/swift-docker"
-  url "https://github.com/iainsmith/swift-docker/archive/0.3.0.tar.gz"
-  sha256 "91e9d721705cd8c3f2aebcfc2912311b75e75976db48486cdcd50d17bfee2cbe"
+  url "https://github.com/iainsmith/swift-docker/archive/0.4.0.tar.gz"
+  sha256 "b5519f390ebf1fff63c05d2936aa068e6a4f7f9bc9f2847acf4d4dfe81e4fa14"
   depends_on :xcode => :build
 
   def install
